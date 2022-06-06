e97c56d4482b9b166244c4806523f4250c2187bc
PatriotRossii <patriotrossii2019@mail.ru>
2022-06-06 07:15:46 +0500

    Minor changes in database schema

--- analyzer/db/alembic/env.py
+++ analyzer/db/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from analyzer.db.core import Base
-from analyzer.db.schema import Item, Category
+from analyzer.db.schema import ShopUnit, PriceUpdate
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- analyzer/db/schema.py
+++ analyzer/db/schema.py
@@ -1,24 +1,25 @@
-from sqlalchemy import MetaData, Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import MetaData, Column, ForeignKey
+from sqlalchemy.types import Boolean, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declared_attr
 
 from .core import Base
 
 
-class ShopUnit(object):
+class ShopUnit(Base):
+    __tablename__ = "shop_units"
+
     id = Column(String, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    
-    @declared_attr
-    def parent(cls):
-        return Column(Integer, ForeignKey("categories.id"), nullable=True)
-
-
-class Category(Base, ShopUnit):
-    __tablename__ = "categories"
-
-
-class Item(Base, ShopUnit):
-    __tablename__ = "items"
-
+    parent = Column(Integer, ForeignKey("shop_units.id"))
     price = Column(Integer)
+    is_category = Column(Boolean)
+
+
+class PriceUpdate(Base):
+    __tablename__ = "price_updates"
+
+    id = Column(Integer, primary_key=True, index=True)
+    unit_id = Column(Integer, ForeignKey("price_updates.id"))
+    price = Column(Integer)
+    date = Column(DateTime)
