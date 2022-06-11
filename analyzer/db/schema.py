1907990ed270e3d2fcf46a9e2c331a063c1332b9
PatriotRossii <patriotrossii2019@mail.ru>
2022-06-11 13:59:03 +0500

    Minor changes in db schema, generate migration

--- analyzer/db/schema.py
+++ analyzer/db/schema.py
@@ -11,7 +11,7 @@ class ShopUnit(Base):
 
     id = Column(String, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    parent = Column(Integer, ForeignKey("shop_units.id"))
+    parent = Column(Integer, ForeignKey("shop_units.id", ondelete="CASCADE"))
     price = Column(Integer)
     is_category = Column(Boolean)
 
@@ -20,6 +20,6 @@ class PriceUpdate(Base):
     __tablename__ = "price_updates"
 
     id = Column(Integer, primary_key=True, index=True)
-    unit_id = Column(Integer, ForeignKey("price_updates.id"))
+    unit_id = Column(Integer, ForeignKey("price_updates.id", ondelete="CASCADE"))
     price = Column(Integer)
     date = Column(DateTime)
