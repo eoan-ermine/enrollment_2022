4b1069789cc4f9d4dd4bc65cdd879125b79b1e11
PatriotRossii <patriotrossii2019@mail.ru>
2022-06-17 21:24:06 +0500

    Make fixtures work well with async

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,32 +1,38 @@
 import pytest
+import pytest_asyncio
 from alembic.command import upgrade
-from fastapi.testclient import TestClient
+from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from analyzer.api.app import app, get_session
-from analyzer.db.core import ASYNCHRONOUS_DATABASE_URL
 
 
 @pytest.fixture
-async def migrated_sqlite(alembic_config, sqlite):
+def migrated_sqlite(alembic_config, sqlite):
     upgrade(alembic_config, "head")
     return sqlite
 
 
-@pytest.fixture
-def session(migrated_sqlite):
-    engine = create_async_engine(ASYNCHRONOUS_DATABASE_URL, future=True)
+@pytest_asyncio.fixture
+async def session(migrated_sqlite):
+    engine = create_async_engine(migrated_sqlite.replace("sqlite", "sqlite+aiosqlite"), future=True)
     SessionLocal = sessionmaker(
         bind=engine, autocommit=False, autoflush=False, expire_on_commit=False, class_=AsyncSession
     )
-    with SessionLocal() as session:
+    async with SessionLocal() as session:
         yield session
 
 
-@pytest.fixture
-def client(session):
-    app.depencdency_overrides[get_session] = session
-    client = TestClient(app)
-    yield client
-    app.depencdency_overrides.clear()
+@pytest_asyncio.fixture
+async def client(session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+    client = AsyncClient(app=app, base_url="http://test")
+    try:
+        yield client
+    finally:
+        await client.aclose()
+    app.dependency_overrides.clear()
