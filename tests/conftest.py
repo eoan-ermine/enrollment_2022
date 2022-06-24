535c2708d92078458742d952468c098e9c2e9dfd
PatriotRossii <patriotrossii2019@mail.ru>
2022-06-24 15:45:50 +0500

    Debug

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from sqlalchemy_utils import create_database, drop_database
 from yarl import URL
 
-from analyzer.db.core import SYNC_DATABASE_URL
+from analyzer.db.core import ANALYZER_PG_PATH, SYNC_DATABASE_URL
 from analyzer.utils.database import make_alembic_config
 
 
@@ -16,8 +16,9 @@ def postgres():
     """
 
     tmp_name = uuid.uuid4().hex
-    print("postgres():", SYNC_DATABASE_URL)
+    print("postgres():", ANALYZER_PG_PATH, SYNC_DATABASE_URL)
     tmp_url = str(URL(SYNC_DATABASE_URL).with_path(tmp_name))
+    print(tmp_url)
     create_database(tmp_url)
 
     try:
